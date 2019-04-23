a5544f84adaa6a2639272a38bfd9d6c8487406cd
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-04-23 09:49:13 -0400

    Add solution to rna-transcription exercise

---
+++ typescript/rna-transcription/rna-transcription.ts
@@ -0,0 +1,26 @@
+type DNANucleotide = 'A' | 'C' | 'G' | 'T'
+type RNANucleotide = 'A' | 'C' | 'G' | 'U'
+
+enum DNA_NUCLEOTIDES {
+    C = 'G',
+    G = 'C',
+    A = 'U',
+    T = 'A'
+}
+
+class Transcriptor {
+    toRna(dna: string) {
+        return dna.split('').map(this.transcribeNucleotide_).join('')
+    }
+
+    private transcribeNucleotide_(nucleotide: DNANucleotide): RNANucleotide {
+        const transcribedNucleotide: RNANucleotide = DNA_NUCLEOTIDES[nucleotide]
+        if (transcribedNucleotide) {
+                return transcribedNucleotide
+        }
+
+        throw new Error('Invalid input DNA.')
+    }
+}
+
+export default Transcriptor
