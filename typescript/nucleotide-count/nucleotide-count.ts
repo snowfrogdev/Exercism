8b07e9b911c15febfdc96a96e32eb87b238c382e
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-04-29 10:37:17 -0400

    add solution to nucleotide-count

--- typescript/nucleotide-count/nucleotide-count.ts
+++ typescript/nucleotide-count/nucleotide-count.ts
@@ -1,6 +1,15 @@
 class NucleotideCount {
-  static nucleotideCounts( /* Parameters go here */ ) {
-    // Your code here
+  static nucleotideCounts(dnaStrand: string) {
+    const count: { [key: string]: number } = { A: 0, C: 0, G: 0, T: 0 }
+
+    Array.from(dnaStrand).forEach((nucleotide) => {
+      if (count[nucleotide] === undefined) {
+        throw new Error('Invalid nucleotide in strand')
+      }
+      count[nucleotide] += 1
+    })
+
+    return count
   }
 }
 
