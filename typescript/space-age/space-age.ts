f2ca96e147bc47ebd9ed730dfd23cd2de22fa463
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-06-01 16:44:46 -0400

    Refactor Space Age to use enum

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,51 +1,59 @@
+const EARTH_YEAR_IN_SECONDS = 31557600
+
+enum OrbitalPeriodInEarthYears {
+    EARTH   = 1,
+    MERCURY = 0.2408467,
+    VENUS   = 0.61519726,
+    MARS    = 1.8808158,
+    JUPITER = 11.862615,
+    SATURN  = 29.447498,
+    URANUS  = 84.016846,
+    NEPTUNE = 164.79132
+}
+
 export default class SpaceAge {
-    static EARTH_YEAR_IN_SECONDS = 31557600
-    constructor(public seconds: number) {}
+    private earthAgeNotRounded: number
+    constructor(public seconds: number) {
+        this.earthAgeNotRounded = this.seconds / EARTH_YEAR_IN_SECONDS
+    }
 
     onEarth(): number {
-        return this.round_(this.getEarthAgeNotRounded_())
+        return this.getSpaceAge(OrbitalPeriodInEarthYears.EARTH)
     }
 
     onMercury(): number {
-        const MERCURY_ORBITAL_PERIOD_IN_EARTH_YEARS = 0.2408467
-        return this.round_(this.getEarthAgeNotRounded_() / MERCURY_ORBITAL_PERIOD_IN_EARTH_YEARS)
+        return this.getSpaceAge(OrbitalPeriodInEarthYears.MERCURY)
     }
 
     onVenus(): number {
-        const VENUS_ORBITAL_PERIOD_IN_EARTH_YEARS = 0.61519726
-        return this.round_(this.getEarthAgeNotRounded_() / VENUS_ORBITAL_PERIOD_IN_EARTH_YEARS)
+        return this.getSpaceAge(OrbitalPeriodInEarthYears.VENUS)
     }
 
     onMars(): number {
-        const MARS_ORBITAL_PERIOD_IN_EARTH_YEARS = 1.8808158
-        return this.round_(this.getEarthAgeNotRounded_() / MARS_ORBITAL_PERIOD_IN_EARTH_YEARS)
+        return this.getSpaceAge(OrbitalPeriodInEarthYears.MARS)
     }
 
     onJupiter(): number {
-        const JUPITER_ORBITAL_PERIOD_IN_EARTH_YEARS = 11.862615
-        return this.round_(this.getEarthAgeNotRounded_() / JUPITER_ORBITAL_PERIOD_IN_EARTH_YEARS)
+        return this.getSpaceAge(OrbitalPeriodInEarthYears.JUPITER)
     }
 
     onSaturn(): number {
-        const SATURN_ORBITAL_PERIOD_IN_EARTH_YEARS = 29.447498
-        return this.round_(this.getEarthAgeNotRounded_() / SATURN_ORBITAL_PERIOD_IN_EARTH_YEARS)
+        return this.getSpaceAge(OrbitalPeriodInEarthYears.SATURN)
     }
 
     onUranus(): number {
-        const URANUS_ORBITAL_PERIOD_IN_EARTH_YEARS = 84.016846
-        return this.round_(this.getEarthAgeNotRounded_() / URANUS_ORBITAL_PERIOD_IN_EARTH_YEARS)
+        return this.getSpaceAge(OrbitalPeriodInEarthYears.URANUS)
     }
 
     onNeptune(): number {
-        const NEPTUNE_ORBITAL_PERIOD_IN_EARTH_YEARS = 164.79132
-        return this.round_(this.getEarthAgeNotRounded_() / NEPTUNE_ORBITAL_PERIOD_IN_EARTH_YEARS)
+        return this.getSpaceAge(OrbitalPeriodInEarthYears.NEPTUNE)
+    }
+
+    private getSpaceAge(orbitalPeriodInEarthYears: number): number {
+        return this.round_(this.earthAgeNotRounded / orbitalPeriodInEarthYears)
     }
 
     private round_(float: number) {
         return parseFloat(float.toFixed(2))
-    }
-
-    private getEarthAgeNotRounded_(): number {
-        return this.seconds / SpaceAge.EARTH_YEAR_IN_SECONDS
     }
 }
