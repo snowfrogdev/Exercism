a21dce9dd940723516eb9c6a81076e5b4ec33975
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-06-12 10:52:13 -0400

    Solve Perfect Number, the right way

--- typescript/perfect-numbers/perfect-numbers.ts
+++ typescript/perfect-numbers/perfect-numbers.ts
@@ -1,14 +1,34 @@
 export default class PerfectNumbers {
   static classify(num: number): string {
-    if (num === 12 || num === 30 || num === 33550335) {
-      return 'abundant'
-    }
-    if (num === 2 || num === 4 || num === 32 || num === 33550337 || num === 1) {
-      return 'deficient'
-    }
-    if (num === 0 || num === -1) {
+    if (num <= 0) {
       throw 'Classification is only possible for natural numbers.'
     }
+
+    const aliquotSum: number = factors(num).reduce((p, c) => p + c) - num
+
+    if (aliquotSum > num) {
+      return 'abundant'
+    }
+    if (aliquotSum < num) {
+      return 'deficient'
+    }
+
     return 'perfect'
   }
+}
+
+function factors(num: number) {
+  const n_factors = []
+
+  for (let i = 1; i <= Math.floor(Math.sqrt(num)); i += 1) {
+    if (num % i === 0) {
+      n_factors.push(i)
+      if (num / i !== i) {
+        n_factors.push(num / i)
+      }
+    }
+  }
+
+  n_factors.sort((a, b) => a - b)
+  return n_factors
 }
