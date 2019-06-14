cac9e2b4b98898cdcf76a955772f6d7e82beeea5
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-06-14 16:19:02 -0400

    Solve grain - optimized

--- typescript/grains/grains.ts
+++ typescript/grains/grains.ts
@@ -1,23 +1,12 @@
 export default class Grains {
-  static square(sqrNum: number, squareValue: number = 1): number {
-    if (sqrNum > 64) {
-      throw 'A chessboard only has 64 squares.'
+  static square(sqrNum: number): number {
+    if (sqrNum <= 0 || sqrNum > 64) {
+      throw 'Square is out of range on a chessboard'
     }
-
-    sqrNum--
-    if (sqrNum === 0) {
-      return squareValue
-    }
-
-    return Grains.square(sqrNum, squareValue *= 2)
+    return (2 ** sqrNum) / 2
   }
 
   static total(): number {
-    let total = 0
-    for (let i = 1; i <= 64; i++) {
-      total += Grains.square(i)
-    }
-    return total
+    return (2 ** 64) - 1
   }
 }
-
