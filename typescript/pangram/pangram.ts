80571e1f1af9654d222aab9a92da09c3fcbcf5a6
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-07-09 20:29:54 -0400

    Refactor pangram following mentor feedback

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,21 +1,13 @@
+const ALPHABET_TEST = /[a-z]/g
+
 export default class Pangram {
     private letters_: Set<String>
-    private readonly ASCII_CODE_FOR_LOWER_CASE_A = 97
-    private readonly ASCII_CODE_FOR_LOWER_CASE_Z = 122
+
     constructor(sentence: string) {
-        this.letters_ = new Set<string>(sentence.toLowerCase())
+        this.letters_ = new Set<string>(sentence.toLowerCase().match(ALPHABET_TEST))
     }
 
     isPangram() {
-        for (let asciiCode = this.ASCII_CODE_FOR_LOWER_CASE_A; asciiCode <= this.ASCII_CODE_FOR_LOWER_CASE_Z; asciiCode++) {
-            if (!this.hasLetter_(asciiCode)) {
-                return false
-            }
-        }
-        return true
-    }
-
-    private hasLetter_(asciiCode: number): boolean {
-        return this.letters_.has(String.fromCharCode(asciiCode))
+        return this.letters_.size === 26
     }
 }
