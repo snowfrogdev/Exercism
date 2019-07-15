554b29460ddc9007b4c0142efb7101d0ad50666a
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-07-15 10:20:13 -0400

    Add solution to roman numerals

--- typescript/roman-numerals/roman-numerals.ts
+++ typescript/roman-numerals/roman-numerals.ts
@@ -1,44 +1,30 @@
-const DECIMAL_POSITION_TO_ROMAN_NUMERAL: {[index: number]: [string, string]} = {  
-  4: ['M', ''],
-  3: ['C', 'D'],
-  2: ['X', 'L'],
-  1: ['I', 'V'],
-  0: ['', '']
-}
-
 class RomanNumerals {
   static roman(num: number) {
-    const numAsString = String(num)
-
     let result = ''
 
-    for (let i = 0; i < numAsString.length; i++) {
-      const decimalPositionIndex = numAsString.length - i;
-      const valueAtDecimalPosition = Number(numAsString[i])
+    const decimalToRomanMap = new Map<number, string>([
+      [1000, 'M'],
+      [900, 'CM'],
+      [500, 'D'],
+      [400, 'CD'],
+      [100, 'C'],
+      [90, 'XC'],
+      [50, 'L'],
+      [40, 'XL'],
+      [10, 'X'],
+      [9, 'IX'],
+      [5, 'V'],
+      [4, 'IV'],
+      [1, 'I']
+    ])
 
-      if (valueAtDecimalPosition === 0) {
-        continue;
-      } else if (valueAtDecimalPosition < 4) {
-        const romanNumeral = DECIMAL_POSITION_TO_ROMAN_NUMERAL[decimalPositionIndex][0];
-        for (let j = 0; j < valueAtDecimalPosition; j++) {
-          result += romanNumeral;
-        }
-      } else if (valueAtDecimalPosition === 4) {
-        result += DECIMAL_POSITION_TO_ROMAN_NUMERAL[decimalPositionIndex][0];
-        result += DECIMAL_POSITION_TO_ROMAN_NUMERAL[decimalPositionIndex][1];
-      } else if (valueAtDecimalPosition === 5) {
-        result += DECIMAL_POSITION_TO_ROMAN_NUMERAL[decimalPositionIndex][1];
-      } else if (valueAtDecimalPosition > 5 && valueAtDecimalPosition < 9) {
-        result += DECIMAL_POSITION_TO_ROMAN_NUMERAL[decimalPositionIndex][1];
-        for (let j = 5; j < valueAtDecimalPosition; j++) {
-          result += DECIMAL_POSITION_TO_ROMAN_NUMERAL[decimalPositionIndex][0];
-        }
-      } else if (valueAtDecimalPosition === 9) {
-        result += DECIMAL_POSITION_TO_ROMAN_NUMERAL[decimalPositionIndex][0];
-        result += DECIMAL_POSITION_TO_ROMAN_NUMERAL[decimalPositionIndex + 1][0];
+    decimalToRomanMap.forEach((roman, decimal) => {
+      let quotient: bigint = BigInt(num) / BigInt(decimal)
+      num = num % decimal
+      while (quotient--) {
+        result += roman
       }
-
-    }
+    })
 
     return result
   }
