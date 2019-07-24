c9139280a3307c3e91b5d903e86aea04038eadad
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2019-07-24 10:26:44 -0400

    Add solution to Matrix

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,7 +1,23 @@
 class Matrix {
-    constructor( /* Parameters go here */ ) {
-        // Your code here
-    }
+  rows: number[][]
+  columns: number[][]
+  constructor(matrix: string) {
+    this.parseMatrix_(matrix)
+  }
+
+  private parseMatrix_(matrix: string): void {
+    this.rows = matrix
+      .split('\n')
+      .map((row) => row.split(' '))
+      .map((row) => row.map(Number))
+
+    this.columns = Array(this.rows.length).fill(undefined).map((_) => [])
+    this.rows.forEach((row, rowIndex) => {
+      row.forEach((value, columnIndex) => {
+        this.columns[columnIndex][rowIndex] = value
+      })
+    })
+  }
 }
 
 export default Matrix
