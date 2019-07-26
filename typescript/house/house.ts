9b32bc5f662f8772c7fa0b472197a9c9e2828716
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2019-07-26 13:33:21 -0400

    Add beer-song exercise

--- typescript/house/house.ts
+++ typescript/house/house.ts
@@ -11,41 +11,41 @@ const syntax = [
   ['kept', 'rooster that crowed in the morn'],
   ['belonged to', 'farmer sowing his corn'],
   ['', 'horse and the hound and the horn']
-]
+];
 
 export default class House {
   static verse(verseNumber: number): string[] {
-    const verse = []
+    const verse = [];
 
-    for (let i = verseNumber - 1; i >= 0 ; i--) {
-      let line = ''
+    for (let i = verseNumber - 1; i >= 0; i--) {
+      let line = '';
 
-      const isFirstLine = i === verseNumber - 1
+      const isFirstLine = i === verseNumber - 1;
       if (isFirstLine) {
-        line += `This is the ${syntax[verseNumber - 1][1]}`
+        line += `This is the ${syntax[verseNumber - 1][1]}`;
       } else {
-        line += `that ${syntax[i][0]} the ${syntax[i][1]}`
+        line += `that ${syntax[i][0]} the ${syntax[i][1]}`;
       }
 
-      const isLastLine = i === 0
+      const isLastLine = i === 0;
       if (isLastLine) {
-        line += '.'
+        line += '.';
       }
 
-      verse.push(line)
+      verse.push(line);
     }
 
-    return verse
+    return verse;
   }
 
   static verses(startVerse: number, endVerse: number): string[] {
-    const verses = []
+    const verses = [];
     for (let i = startVerse; i <= endVerse; i++) {
-      verses.push(House.verse(i))
+      verses.push(House.verse(i));
       if (i !== endVerse) {
-        verses.push('')
+        verses.push('');
       }
     }
-    return verses.flat()
+    return verses.flat();
   }
 }
