f0fc35559a1eaecedb6233d6acceab42c081f094
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-17 19:55:42 -0400

    Pass all tests for beer-song in rust

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,7 +1,16 @@
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    match n {
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
+        2 => String::from("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"),
+        _ => format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {m} bottles of beer on the wall.\n", n = n, m = n - 1)
+    }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    if start == end {
+        return verse(end);
+    }
+
+    return verse(start) + "\n" + &sing(start - 1, end);
 }
