ca259fa1565df301898373069d01b419e6b14629
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-17 20:20:22 -0400

    (beer-song.rs) split lines in match expression

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,9 +1,16 @@
 pub fn verse(n: u32) -> String {
     match n {
-        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
-        1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
-        2 => String::from("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"),
-        _ => format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {m} bottles of beer on the wall.\n", n = n, m = n - 1)
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\n") + 
+            "Go to the store and buy some more, 99 bottles of beer on the wall.\n",
+
+        1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\n") +
+            "Take it down and pass it around, no more bottles of beer on the wall.\n",
+
+        2 => String::from("2 bottles of beer on the wall, 2 bottles of beer.\n") +
+            "Take one down and pass it around, 1 bottle of beer on the wall.\n",
+            
+        _ => format!("{n} bottles of beer on the wall, {n} bottles of beer.\n", n = n) + 
+            &format!("Take one down and pass it around, {} bottles of beer on the wall.\n", n - 1)
     }
 }
 
