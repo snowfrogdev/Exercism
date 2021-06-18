0182024254f3588c0f5fb0a4ca149ebabffe2f63
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-18 14:54:23 -0400

    Download gigasecond for rust

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    if list.is_empty() {
+        return String::new();
+    }
+    let lines = build_lines(list, String::new());
+    lines + &format!("And all for the want of a {}.", list[0])
+}
+
+fn build_lines(list: &[&str], proverb: String) -> String {
+    if list.len() == 1 {
+        return proverb;
+    }
+
+    let line = format!("For want of a {} the {} was lost.", list[0], list[1]);
+    build_lines(&list[1..], proverb + &line + "\n")
 }
