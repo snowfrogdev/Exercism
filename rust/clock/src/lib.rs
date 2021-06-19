70bb07b616ad432f9b942c9fffff9adc946af59d
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-19 17:10:28 -0400

    (clock.rs) first implementation

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,26 @@
-pub struct Clock;
+use std::fmt::{Debug, Display};
+
+use chrono::NaiveTime;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Clock {
+    time: NaiveTime,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock {
+            time: NaiveTime::from_hms(hours as u32, minutes as u32, 0),
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         unimplemented!("Add {} minutes to existing Clock time", minutes);
+    }
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.time.format("%H:%M").to_string())
     }
 }
