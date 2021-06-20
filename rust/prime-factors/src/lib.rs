a40f56b44e5fad9e7e033e223cf252a871cc4128
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-20 14:30:51 -0400

    (prime-factore.rs) refactor for readability

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,18 +1,22 @@
 pub fn factors(mut n: u64) -> Vec<u64> {
     let mut prime_factors = Vec::<u64>::new();
+
     while n % 2 == 0 {
         prime_factors.push(2);
         n /= 2;
     }
 
-    for i in (3..=(n as f64).sqrt() as u64).step_by(2) {
+    let range = 3..=(n as f64).sqrt() as u64;
+    for i in range.step_by(2) {
         while n % i == 0 {
             prime_factors.push(i);
             n /= i;
         }
     }
 
-    if n > 2 { prime_factors.push(n) }
+    if n > 2 {
+        prime_factors.push(n)
+    }
 
     prime_factors
 }
