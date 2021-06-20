67c5796f63c2d53b8e32016dcb2147613f3f007e
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-20 12:01:02 -0400

    (prime-factors.rs) pass all tests

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,18 @@
-pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+pub fn factors(mut n: u64) -> Vec<u64> {
+    let mut prime_factors = Vec::<u64>::new();
+    while n % 2 == 0 {
+        prime_factors.push(2);
+        n /= 2;
+    }
+
+    for i in (3..=(n as f64).sqrt() as u64).step_by(2) {
+        while n % i == 0 {
+            prime_factors.push(i);
+            n /= i;
+        }
+    }
+
+    if n > 2 { prime_factors.push(n) }
+
+    prime_factors
 }
