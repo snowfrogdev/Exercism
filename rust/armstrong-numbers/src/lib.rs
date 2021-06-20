4edb3777946d822fa1b77ceb58e1860fad7f4565
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-20 15:42:42 -0400

    (armstrong-numbers.rs) pass all tests

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,25 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    num == required_sum(num)
+}
+
+fn required_sum(mut num: u32) -> u32 {
+    let count = count_digits(num);
+
+    let mut sum = 0;
+    while num > 0 {
+        sum += (num % 10).pow(count);
+        num /= 10;
+    }
+
+    sum
+}
+
+fn count_digits(mut num: u32) -> u32 {
+    let mut i = 0;
+    while num > 0 {
+        num /= 10;
+        i += 1;
+    }
+
+    i
 }
