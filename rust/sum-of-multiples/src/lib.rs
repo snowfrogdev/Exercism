d214df17aa282b3a992f152228509f5ac235107b
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-19 21:07:37 -0400

    (sum-of-multiples.rs) pass all test

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,7 +1,15 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    let mut multiples = HashSet::new();
+
+    for factor in factors {
+        let mut multiple = *factor;
+        while multiple != 0 && multiple < limit {
+            multiples.insert(multiple);
+            multiple += factor;
+        }
+    }
+
+    multiples.iter().sum()
 }
