2b919f2091d9b3724cd885de6639c8967814c99f
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-20 20:07:24 -0400

    (matching-brackets.rs) pass all tests

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,6 +1,30 @@
+use std::collections::HashMap;
+
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut brackets_map = HashMap::new();
+    brackets_map.insert('[', ']');
+    brackets_map.insert('{', '}');
+    brackets_map.insert('(', ')');
+
+    let mut stack = Vec::<char>::new();
+    
+    let brackets = string
+        .chars()
+        .filter(|c| brackets_map.contains_key(c) || brackets_map.values().any(|v| c == v));
+    for bracket in brackets {
+        if brackets_map.contains_key(&bracket) {
+            stack.push(bracket);
+        } else {
+            match brackets_map.get(&stack.pop().unwrap_or_default()) {
+                Some(&x) => {
+                    if x != bracket {
+                        return false;
+                    }
+                }
+                None => return false,
+            }
+        }
+    }
+
+    stack.is_empty()
 }
