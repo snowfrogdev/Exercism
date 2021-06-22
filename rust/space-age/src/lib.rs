be08e4808d4894356e3699a370afacdcc58e00db
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-22 16:52:13 -0400

    (space-age.rs) pass all tests

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,38 +1,35 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration(f64);
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        const SECONDS_IN_EARTH_YEAR: f64 = 31_557_600f64;
+        Duration(s as f64 / SECONDS_IN_EARTH_YEAR)
     }
 }
 
-pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
-    }
+macro_rules! planets {
+    ($($name:ident => $earth_years:expr),*) => {
+            $(
+                pub struct $name;
+
+                impl $name {
+                    pub fn years_during(d: &Duration) -> f64 {
+                        d.0 / $earth_years as f64
+                    }
+                }
+            )*
+
+    };
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
-
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+planets!(
+    Earth => 1,
+    Mercury => 0.2408467,
+    Venus => 0.61519726,
+    Mars => 1.8808158,
+    Jupiter => 11.862615,
+    Saturn => 29.447498,
+    Uranus => 84.016846,
+    Neptune => 164.79132
+);
