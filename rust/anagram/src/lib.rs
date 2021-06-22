c6ff9f88281591d907e8e25aed7ef2017d7a8e35
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-21 21:49:07 -0400

    (anagram.rs) pass all tests

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,20 @@
+use itertools::Itertools;
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let word = word.to_lowercase();
+    let sorted_word = sort(&word);
+
+    possible_anagrams
+        .iter()
+        .filter(|candidate| {
+            let candidate = candidate.to_lowercase();
+            sort(&candidate) == sorted_word && candidate != word
+        })
+        .cloned()
+        .collect()
+}
+
+fn sort(word: &str) -> Vec<char> {
+    word.chars().sorted_unstable().collect_vec()
 }
