017f35d3aeec54b78f19d2820afddfc646469f92
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-22 22:21:37 -0400

    (sublist.rs) refactor to use custom trait

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -8,19 +8,24 @@ pub enum Comparison {
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
     match first_list.len() {
-        len if len == second_list.len() && first_list.iter().eq(second_list.iter()) => Comparison::Equal,
-        len if len > second_list.len() && second_list.is_sublist_of(first_list) => Comparison::Superlist,
+        _ if first_list == second_list => Comparison::Equal,
         _ if first_list.is_sublist_of(second_list) => Comparison::Sublist,
-        _ => Comparison::Unequal
+        _ if first_list.is_superlist_of(second_list) => Comparison::Superlist,
+        _ => Comparison::Unequal,
     }
 }
 
 trait Sublist<T: PartialEq> {
     fn is_sublist_of(&self, other: &[T]) -> bool;
+    fn is_superlist_of(&self, other: &[T]) -> bool;
 }
 
 impl<T: PartialEq> Sublist<T> for [T] {
     fn is_sublist_of(&self, other: &[T]) -> bool {
         self.is_empty() || other.windows(self.len()).any(|window| window == self)
+    }
+
+    fn is_superlist_of(&self, other: &[T]) -> bool {
+        self.len() > other.len() && other.is_sublist_of(self)
     }
 }
