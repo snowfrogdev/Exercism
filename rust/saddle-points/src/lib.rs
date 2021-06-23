b271f449e83f13427bd0c819f7e9928f57283da0
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-23 00:37:21 -0400

    (saddle-points.rs) pass all tests

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,6 +1,17 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut points = Vec::new();
+
+    for (row_index, row) in input.iter().enumerate() {
+        let largest = row.iter().max().unwrap_or(&0);
+
+        for (col_index, value) in row.iter().enumerate() {
+            let smallest = (0..input.len()).map(|i| input[i][col_index]).min().unwrap();
+
+            if value == largest && value == &smallest {
+                points.push((row_index, col_index));
+            }
+        }
+    }
+
+    points
 }
