f3362a8110cc7ef1f3eb7a7197ef2020978ccba0
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-23 16:44:05 -0400

    (isogram.rs) pass all tests

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    let mut letters: Vec<char> = candidate
+        .to_lowercase()
+        .chars()
+        .filter(|char| char.is_alphabetic())
+        .collect();
+    letters.sort_unstable();
+    let char_count = letters.len();
+    letters.dedup();
+    letters.len() == char_count
 }
