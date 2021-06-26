d4ba144e948fa833b096e608a63cdc4914732f69
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-26 13:37:44 -0400

    (run-length-encodings.rs) pass all encoding tests

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -1,5 +1,26 @@
 pub fn encode(source: &str) -> String {
-    unimplemented!("Return the run-length encoding of {}.", source);
+    let mut result = String::new();
+
+    let mut last: Option<char> = None;
+    let mut last_count = 0;
+    for c in source.chars().chain("#".chars()) {
+        if Some(c) == last {
+            last_count += 1;
+            continue;
+        }
+
+        if last_count == 1 {
+            result.push(last.unwrap());
+        } else if last_count > 1 {
+            result.push_str(&last_count.to_string());
+            result.push(last.unwrap());
+        }
+
+        last = Some(c);
+        last_count = 1;
+    }
+
+    result
 }
 
 pub fn decode(source: &str) -> String {
