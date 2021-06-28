c68262bf2266377858daecd14d36ad3ea329d321
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-27 21:45:08 -0400

    (perfect-numbers.rs) pass all tests

--- rust/perfect-numbers/src/lib.rs
+++ rust/perfect-numbers/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Classification {
     Abundant,
@@ -6,5 +8,20 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    if num == 0 {
+        return None;
+    }
+
+    let sum: u64 = factors(num).iter().filter(|&&n| n != num).sum();
+
+    match sum.cmp(&num) {
+        cmp::Ordering::Equal => Some(Classification::Perfect),
+        cmp::Ordering::Greater => Some(Classification::Abundant),
+        cmp::Ordering::Less => Some(Classification::Deficient),
+    }
 }
+
+fn factors(n: u64) -> Vec<u64> {
+    (1..n).filter(|i| n % i == 0).collect()
+}
+
