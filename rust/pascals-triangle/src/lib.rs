ca6dba5dae8e66b640d6154b3a57eea3b2b41041
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-29 12:22:37 -0400

    (pascals-triangle.rs) pass all tests

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -1,11 +1,22 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle(Vec<Vec<u32>>);
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {} rows", row_count);
+        PascalsTriangle((0..row_count).map(PascalsTriangle::calc_row).collect())
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        self.0.clone()
+    }
+
+    fn calc_row(row_index: u32) -> Vec<u32> {
+        let mut result = vec![1];
+
+        for i in 1..=row_index {
+            let &last = result.last().unwrap();
+            result.push((last * (row_index + 1 - i)) / i);
+        }
+
+        result
     }
 }
