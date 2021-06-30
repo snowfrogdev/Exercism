8486b398757d03637a2c50c14cbe4ebbf0de8524
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-30 15:49:29 -0400

    (acronym.rs) pass all tests

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,3 +1,9 @@
+use fancy_regex::Regex;
+
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    let re = Regex::new(r"\b[A-Za-z](?<!'[A-Za-z])|[A-Z](?<![A-Z]{2})").unwrap();
+
+    re.find_iter(phrase)
+        .map(|x| x.unwrap().as_str().to_ascii_uppercase())
+        .collect()
 }
