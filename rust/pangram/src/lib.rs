befbdcf31150d8f6a219280bc38aeb8b6b13ffa3
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-30 11:31:28 -0400

    (pangram.rs) pass all tests

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,4 +1,10 @@
-/// Determine whether a sentence is a pangram.
+use lazy_static::lazy_static;
+use fancy_regex::Regex;
+
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"([a-z])(?!.*\1)").unwrap();
+    }
+    
+    RE.find_iter(sentence.to_ascii_lowercase().as_str()).count() == 26
 }
