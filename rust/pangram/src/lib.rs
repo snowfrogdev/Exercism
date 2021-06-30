f5223a47bd961ff3b3f1d361f72f0e35511f88b2
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-30 11:33:33 -0400

    (pangram.rs) fix formatting

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,10 +1,10 @@
-use lazy_static::lazy_static;
 use fancy_regex::Regex;
+use lazy_static::lazy_static;
 
 pub fn is_pangram(sentence: &str) -> bool {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"([a-z])(?!.*\1)").unwrap();
     }
-    
+
     RE.find_iter(sentence.to_ascii_lowercase().as_str()).count() == 26
 }
