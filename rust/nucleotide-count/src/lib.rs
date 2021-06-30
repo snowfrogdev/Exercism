82ee2c61a48ce903e4cbd70721f5db4ad9b7aea8
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-30 13:36:18 -0400

    (nucleotide-counts.rs) pass all tests

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,16 +1,31 @@
 use std::collections::HashMap;
 
+static VALID: [char; 4] = ['A', 'C', 'G', 'T'];
+
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    unimplemented!(
-        "How much of nucleotide type '{}' is contained inside DNA string '{}'?",
-        nucleotide,
-        dna
-    );
+    if VALID.contains(&nucleotide) {
+        dna.chars().fold(Ok(0), |p, c| {
+            match c {
+                'A' | 'C' | 'G' | 'T' if c == nucleotide => p.map(|count| count + 1),
+                'A' | 'C' | 'G' | 'T' => p,
+                _ => Err(c),
+            }
+        })
+    } else {
+        Err(nucleotide)
+    }
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    unimplemented!(
-        "How much of every nucleotide type is contained inside DNA string '{}'?",
-        dna
-    );
+    let mut map = HashMap::new();
+    for &n in VALID.iter() {
+        match count(n, dna) {
+            Ok(count) => {
+                map.insert(n, count);
+            }
+            Err(c) => return Err(c),
+        }
+    }
+
+    Ok(map)
 }
