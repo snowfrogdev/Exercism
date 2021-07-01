522fd38ea748e1cc42a03902a06e65e5d24a1616
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-06-30 21:48:46 -0400

    (rna-transcription.rs) pass all tests

--- rust/rna-transcription/src/lib.rs
+++ rust/rna-transcription/src/lib.rs
@@ -1,21 +1,63 @@
 #[derive(Debug, PartialEq)]
-pub struct Dna;
+pub struct Dna(String);
 
 #[derive(Debug, PartialEq)]
-pub struct Rna;
+pub struct Rna(String);
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        unimplemented!("Construct new Dna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", dna);
+        dna.chars()
+            .enumerate()
+            .fold(Ok(String::new()), |p, c| match c {
+                (_, c) if ['A', 'C', 'G', 'T'].contains(&c) => p.map(|mut s| {
+                    s.push(c);
+                    s
+                }),
+                (i, _) => {
+                    if p.is_err() {
+                        p
+                    } else {
+                        Err(i)
+                    }
+                }
+            })
+            .map(Dna)
     }
 
     pub fn into_rna(self) -> Rna {
-        unimplemented!("Transform Dna {:?} into corresponding Rna", self);
+        let rna = self
+            .0
+            .chars()
+            .map(|c| match c {
+                'G' => 'C',
+                'C' => 'G',
+                'T' => 'A',
+                'A' => 'U',
+                _ => panic!(),
+            })
+            .collect::<String>();
+
+        Rna(rna)
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        unimplemented!("Construct new Rna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", rna);
+        rna.chars()
+            .enumerate()
+            .fold(Ok(String::new()), |p, c| match c {
+                (_, c) if ['A', 'C', 'G', 'U'].contains(&c) => p.map(|mut s| {
+                    s.push(c);
+                    s
+                }),
+                (i, _) => {
+                    if p.is_err() {
+                        p
+                    } else {
+                        Err(i)
+                    }
+                }
+            })
+            .map(Rna)
     }
 }
