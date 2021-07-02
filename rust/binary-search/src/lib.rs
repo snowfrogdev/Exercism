5a147c2760688111bb0aa7d33c96a41837c44692
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-07-02 16:23:19 -0400

    (binary-search.rs) pass all tests

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,7 +1,11 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+pub fn find<A: AsRef<[T]>, T: Ord>(array_like: A, key: T) -> Option<usize> {
+    let array = array_like.as_ref();
+    let mid = array.len() / 2;
+    match key.cmp(array.get(mid)?) {
+        std::cmp::Ordering::Less => find(&array[0..mid], key),
+        std::cmp::Ordering::Equal => Some(mid),
+        std::cmp::Ordering::Greater => {
+            find(&array[mid + 1..], key).map(|i| mid + 1 + i)
+        }
+    }
 }
