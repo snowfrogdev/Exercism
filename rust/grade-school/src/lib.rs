2b31213a1cfcee2d6292402c66e1172ab4425d4a
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-07-02 14:02:06 -0400

    (grade-school.rs) pass all tests

--- rust/grade-school/src/lib.rs
+++ rust/grade-school/src/lib.rs
@@ -1,30 +1,31 @@
-// This annotation prevents Clippy from warning us that `School` has a
-// `fn new()` with no arguments, but doesn't implement the `Default` trait.
-//
-// Normally, it's good practice to just do what Clippy tells you, but in this
-// case, we want to keep things relatively simple. The `Default` trait is not the point
-// of this exercise.
 #[allow(clippy::new_without_default)]
-pub struct School {}
+pub struct School<'a>(Vec<(u32, &'a str)>);
 
-impl School {
-    pub fn new() -> School {
-        unimplemented!()
+impl<'a> School<'a> {
+    pub fn new() -> School<'a> {
+        School(Vec::new())
     }
 
-    pub fn add(&mut self, grade: u32, student: &str) {
-        unimplemented!("Add {} to the roster for {}", student, grade)
+    pub fn add(&mut self, grade: u32, student: &'a str) {
+        self.0.push((grade, student));
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
+        let mut grades: Vec<u32> = self.0.iter().map(|(grade, _)| *grade).collect();
+        grades.sort_unstable();
+        grades.dedup();
+        grades
     }
 
-    // If `grade` returned a reference, `School` would be forced to keep a `Vec<String>`
-    // internally to lend out. By returning an owned vector of owned `String`s instead,
-    // the internal structure can be completely arbitrary. The tradeoff is that some data
-    // must be copied each time `grade` is called.
     pub fn grade(&self, grade: u32) -> Vec<String> {
-        unimplemented!("Return the list of students in {}", grade)
+        let mut students: Vec<String> = self
+            .0
+            .iter()
+            .filter(|(g, _)| *g == grade)
+            .map(|(_, name)| name.to_string())
+            .collect();
+
+        students.sort_unstable();
+        students
     }
 }
