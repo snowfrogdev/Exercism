5ff017bc8e5857d00a7fcb54f5dd1583b2dc637a
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-07-02 12:11:50 -0400

    (all-your-base.rs) pass all tests

--- rust/all-your-base/src/lib.rs
+++ rust/all-your-base/src/lib.rs
@@ -5,42 +5,44 @@ pub enum Error {
     InvalidDigit(u32),
 }
 
-///
-/// Convert a number between two bases.
-///
-/// A number is any slice of digits.
-/// A digit is any unsigned integer (e.g. u8, u16, u32, u64, or usize).
-/// Bases are specified as unsigned integers.
-///
-/// Return an `Err(.)` if the conversion is impossible.
-/// The tests do not test for specific values inside the `Err(.)`.
-///
-///
-/// You are allowed to change the function signature as long as all test still pass.
-///
-///
-/// Example:
-/// Input
-///   number: &[4, 2]
-///   from_base: 10
-///   to_base: 2
-/// Result
-///   Ok(vec![1, 0, 1, 0, 1, 0])
-///
-/// The example corresponds to converting the number 42 from decimal
-/// which is equivalent to 101010 in binary.
-///
-///
-/// Notes:
-///  * The empty slice ( "[]" ) is equal to the number 0.
-///  * Never output leading 0 digits, unless the input number is 0, in which the output must be `[0]`.
-///    However, your function must be able to process input with leading 0 digits.
-///
 pub fn convert(number: &[u32], from_base: u32, to_base: u32) -> Result<Vec<u32>, Error> {
-    unimplemented!(
-        "Convert {:?} from base {} to base {}",
-        number,
-        from_base,
-        to_base
-    )
+    if let Err(error) = validate(from_base, to_base, number) {
+        return Err(error);
+    }
+
+    let number_in_base_10: u32 = number
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(index, digit)| digit * from_base.pow(index as u32))
+        .sum();
+
+    let mut result = Vec::new();
+    let mut quotient = number_in_base_10;
+    loop {
+        result.push(quotient % to_base);
+        quotient /= to_base;
+        if quotient <= 0 {
+            break;
+        }
+    }
+    result.reverse();
+
+    Ok(result)
+}
+
+fn validate(from_base: u32, to_base: u32, number: &[u32]) -> Result<(), Error> {
+    if from_base < 2 {
+        return Err(Error::InvalidInputBase);
+    }
+    if to_base < 2 {
+        return Err(Error::InvalidOutputBase);
+    }
+    number.iter().fold(Ok(()), |result, &digit| {
+        if digit >= from_base {
+            Err(Error::InvalidDigit(digit))
+        } else {
+            result
+        }
+    })
 }
